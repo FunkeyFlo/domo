16b87997ea40830cff4d89d211c56e3bd845bc3c
funkeyflo <florentijncornet@gmail.com>
2015-07-01 11:18:35 +0200

    Specify Gordijn

--- socketing/requestHandler.js
+++ socketing/requestHandler.js
@@ -5,12 +5,98 @@ module.exports = function (io) {
 
     io.on('connection', function (socket) {
         socket.on('request', function (data) {
-            console.log('======================== DIT KWAM ER BINNEN ========================')
-            console.log(data);
-            models.Command.find({
-                where: {text: data.command},
-                include: [models.Task]
-            }).then(function (command) {
+
+
+            // CURTAIN ---------------------------------------------
+            if(data.command.indexOf('gordijn') > -1) {
+                if (data.command == 'gordijn open') {
+                    if (!curtainInUse) {
+                        if (!curtainOpen) {
+                            // Code that is executed after timer
+                            setTimeout(function () {
+                                curtainInUse = false;
+                                curtainOpen = true;
+                                socket.emit('message', {
+                                    message: 'Curtain is now open!'
+                                });
+                            }, 38 * 1000);
+                            //code that will be executed immediately
+                            curtainInUse = true;
+                            exec('sudo python scripts/omhoog.py', function (error, stdout, stderr) {
+                                if (stdout !== null) {
+                                    console.log('stdout: ' + stdout);
+                                }
+                                if (stderr !== null) {
+                                    console.log('stderr: ' + stderr);
+                                }
+                                if (error !== null) {
+                                    console.log('exec error: ' + error);
+                                }
+                            });
+                            socket.emit('message', {
+                                message: 'Curtain is being opened!'
+                            });
+                            // curtainOpen Check
+                        } else {
+                            socket.emit('message', {
+                                message: 'Curtain is already open!'
+                            });
+                        }
+                        // Curtain in use check
+                    } else {
+                        socket.emit('message', {
+                            message: 'Curtain is already in use!'
+                        });
+                    }
+                } else {
+
+                }
+
+                if (data.command == 'gordijn dicht') {
+                    if (!curtainInUse) {
+                        if (curtainOpen) {
+                            // Code that is executed after timer
+                            setTimeout(function () {
+                                curtainInUse = false;
+                                curtainOpen = false;
+                                socket.emit('message', {
+                                    message: 'Curtain has been closed!'
+                                });
+                            }, 38 * 1000);
+                            //code that will be executed immediately
+                            curtainInUse = true;
+                            exec('sudo python scripts/omlaag.py', function (error, stdout, stderr) {
+                                if (stdout !== null) {
+                                    console.log('stdout: ' + stdout);
+                                }
+                                if (stderr !== null) {
+                                    console.log('stderr: ' + stderr);
+                                }
+                                if (error !== null) {
+                                    console.log('exec error: ' + error);
+                                }
+                            });
+                            socket.emit('message', {
+                                message: 'Curtain is being closed'
+                            });
+                            // curtainOpen Check
+                        } else {
+                            socket.emit('message', {
+                                message: 'Curtain has been closed!'
+                            });
+                        }
+                        // Curtain in use check
+                    } else {
+                        socket.emit('message', {
+                            message: 'Curtain is already in use!'
+                        });
+                    }
+                }
+            } else {
+                models.Command.find({
+                    where: {text: data.command},
+                    include: [models.Task]
+                }).then(function (command) {
                     var timeTrack = 0;
 
                     var tasks = command.Tasks;
@@ -23,12 +109,13 @@ module.exports = function (io) {
                     socket.emit('message', {
                         message: command.response
                     });
-            }).catch(function(err){
-                console.log(err);
-                socket.emit('message', {
-                    message: "commando: " + data.command + " is niet bekend"
+                }).catch(function(err){
+                    console.log(err);
+                    socket.emit('message', {
+                        message: "commando: " + data.command + " is niet bekend"
+                    });
                 });
-            });
+            }
         });
     });
 }
