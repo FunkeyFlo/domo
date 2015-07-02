ba88a077314eb2b438b579ddb86491914a81979d
funkeyflo <florentijncornet@gmail.com>
2015-07-02 22:55:26 +0200

    kaku fix

--- socketing/requestHandler.js
+++ socketing/requestHandler.js
@@ -11,6 +11,7 @@ module.exports = function (io) {
 
         // KAKU Configurer
         socket.on('kaku', function (data) {
+            console.log('received: ' + data);
             executeCommand(data);
         });
 
